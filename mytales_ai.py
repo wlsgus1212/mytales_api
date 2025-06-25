ad7406621243948b9e15b2894c3bc6658d3f0aa1
wlsgus1212 <wlsgus0130@gmail.com>
2025-06-25 23:24:24 +0900

    🐍 Flask 코드 수정: API 로직 업데이트

--- mytales_ai.py
+++ mytales_ai.py
@@ -7,7 +7,7 @@ import os
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def root():
